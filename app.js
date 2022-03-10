e8666242a194260339f8993f26151fc6a22c53d2
Vittorio <90337009+vittoopugliese@users.noreply.github.com>
2022-03-10 16:48:44 -0300

    imagenes agregadas

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// }
+
 const toggleNav = document.querySelectorAll('#toggle-button')[0]
 const navLinks = document.querySelectorAll('#nav-links')[0]
-
-toggleNav.addEventListener("click", () => {
+toggleNav.addEventListener('click', () => {
     navLinks.classList.toggle('active')
 })
 
+const logo = document.querySelectorAll('#logo')
+const logoText = document.querySelectorAll('#logo-text')
+
+logo[0].onmouseover = function(){
+    logoText[0].style.display = 'flex'
+};
+logo[0].onmouseleave = function(){
+    logoText[0].style.display = 'none'
+};
+
+logo[1].onmouseover = function(){
+    logoText[1].style.display = 'flex'
+};
+logo[1].onmouseleave = function(){
+    logoText[1].style.display = 'none'
+};
