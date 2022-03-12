cb58054dbad73022960449d05e15bb7c61647555
Vittorio <90337009+vittoopugliese@users.noreply.github.com>
2022-03-12 00:01:39 -0300

    footer agrega2

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
+// reload to 0:0
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// }
 
+// toggle nav
 const toggleNav = document.querySelectorAll('#toggle-button')[0]
 const navLinks = document.querySelectorAll('#nav-links')[0]
 toggleNav.addEventListener('click', () => {
     navLinks.classList.toggle('active')
 })
 
+// logo component
+class techno extends HTMLElement{
+    constructor(){
+        super();
+        this.name;
+        this.description;
+        this.img;
+    }
+
+    static get observedAttributes(){
+        return ['name', 'description', 'img'];
+    }
+
+    attributeChangedCallback(nameAttr, oldValue, newValue){
+        switch(nameAttr){
+            case "name":
+                this.name = newValue;
+            break;
+            case "description":
+                this.description = newValue;
+            break;
+            case "img":
+                this.img = newValue;
+            break;
+        }
+    }
+
+    connectedCallback(){
+        this.innerHTML = `
+        <div id="logo-container">
+            <div id="logo">
+                <img draggable="false" src="${this.img}">
+                <h5>${this.name}</h5>
+            </div>
+            <div id="logo-text">
+            <p>${this.description}</p>
+        </div>
+        </div>`;
+    }
+}
+window.customElements.define("tech-no", techno);
+
+// onhover onleave
 const logo = document.querySelectorAll('#logo')
 const logoText = document.querySelectorAll('#logo-text')
 
@@ -19,3 +64,37 @@ for (let i=0; i<logo.length; i++){
         logoText[i].style.display = 'none'
     });
 }
+
+// make full footer vectorsss
+const iso1 = document.querySelector('#iso-1')
+const iso2 = document.querySelector('#iso-2')
+const iso3 = document.querySelector('#iso-3')
+const iso4 = document.querySelector('#iso-4')
+
+iso1.addEventListener('mouseover', function(){
+    iso1.src = 'files/svgs/lilleno.svg'
+});
+iso1.addEventListener('mouseleave', function(){
+    iso1.src = 'files/svgs/livacio.svg'
+});
+
+iso2.addEventListener('mouseover', function(){
+    iso2.src = 'files/svgs/gitlleno.svg'
+});
+iso2.addEventListener('mouseleave', function(){
+    iso2.src = 'files/svgs/gitvacio.svg'
+});
+
+iso3.addEventListener('mouseover', function(){
+    iso3.src = 'files/svgs/gmaillleno.svg'
+});
+iso3.addEventListener('mouseleave', function(){
+    iso3.src = 'files/svgs/gmailvacio.svg'
+});
+
+iso4.addEventListener('mouseover', function(){
+    iso4.src = 'files/svgs/whatsapplleno.svg'
+});
+iso4.addEventListener('mouseleave', function(){
+    iso4.src = 'files/svgs/wspvacio.svg'
+});
